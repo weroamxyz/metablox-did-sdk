c4f789de0b9159cec55bbc0bf8fe30af2a0f02e7
VillageSecretary2020 <93245498+VillageSecretary2020@users.noreply.github.com>
2022-04-14 17:03:15 +0800

    Add files via upload

--- did_wallet/did_wallet.h
+++ did_wallet/did_wallet.h
@@ -1,7 +1,7 @@
 #ifndef __DID_WALLET_H__
 #define __DID_WALLET_H__
 
-#include "did.h"
+#include "did/did.h"
 
 typedef void* wallet_handle;
 
@@ -15,6 +15,8 @@ void            wallet_handle_destroy(wallet_handle handle);
 int             wallet_store_did(wallet_handle wallet, did_handle did, const char* name, const char* password);
 did_handle      wallet_load_did(wallet_handle wallet, const char* name, const char* password); 
 
+void wallet_export_did(wallet_handle wallet, const char *name, char *path);
+void wallet_import_did(wallet_handle wallet, char *path, const char *name, const char *password);
 #ifdef __cplusplus
 }
 #endif 
