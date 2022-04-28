2b21d0b750d654279abf93e8dec7df86a04187a6
VillageSecretary2020 <93245498+VillageSecretary2020@users.noreply.github.com>
2022-04-28 14:57:03 +0800

    Add files via upload

--- did_demo/did_demo/main.c
+++ did_demo/did_demo/main.c
@@ -10,14 +10,15 @@
 #include "did/did.h"
 #include "string.h"
 
-int main(int argc, const char * argv[]) {
+int main(int argc, const char *argv[])
+{
     // insert code here...
     char buffer1[2048] = {0};
     char buffer2[2048] = {0};
     size_t buff_len = 2048;
     did_handle did = did_create("secp256k1", NULL);
 
-    wallet_handle wallet= wallet_handle_create("test-did", "/home/meiqiu/doc/did_sdk-main2/build/test");
+    wallet_handle wallet = wallet_handle_create("test-did", "/home/meiqiu/doc/did_sdk-main2/build/test");
 
     wallet_store_did(wallet, did, "test-did", "12345678");
 
@@ -28,35 +29,35 @@ int main(int argc, const char * argv[]) {
     did_serialize(did2, buffer2, buff_len);
 
     char sig[64] = {0};
-    int did_sign_result=did_sign(did, "Hello, World!", strlen("Hello, World!"), sig, 64);
+    int did_sign_result = did_sign(did, "Hello, World!", strlen("Hello, World!"), sig, 64);
 
-    did_meta_t* did2_meta = did_to_did_meta(did2);
+    did_meta_t *did2_meta = did_to_did_meta(did2);
 
     int verify = did_verify(did2_meta->did_keys, "Hello, World!", strlen("Hello, World!"), sig, 64);
 
-    //2022.4.25
-    char prikey[128]={0};
-    did_export_prikey(did,prikey);
-    printf("\n prikey:%s",prikey);
+    // 2022.4.25
+    char prikey[128] = {0};
+    did_export_prikey(did, prikey);
+    printf("\n prikey:%s", prikey);
 
-    did_handle did6=did_import_privkey(prikey);
+    did_handle did6 = did_import_privkey(prikey);
     char buffer6[2048] = {0};
     did_serialize(did6, buffer6, buff_len);
-    printf("\n did:%p\n did buffer:%s",did,buffer1);
+    printf("\n did:%p\n did buffer:%s", did, buffer1);
     printf("\n compare");
-    printf("\n did6:%p\n did6 buffer:%s",did6,buffer6);
+    printf("\n did6:%p\n did6 buffer:%s", did6, buffer6);
 
-    wallet_change_name(wallet,"test-did","haha");
-    did_handle did3=wallet_load_did(wallet,"haha","12345678");
+    wallet_change_name(wallet, "test-did", "haha");
+    did_handle did3 = wallet_load_did(wallet, "haha", "12345678");
     char buffer3[2048] = {0};
     did_serialize(did3, buffer3, buff_len);
-    printf("\n did3:%p\n did change name:%s",did3,buffer3);
+    printf("\n did3:%p\n did change name:%s", did3, buffer3);
 
-    if(wallet_change_password(wallet,"haha","1234","14725836")==-1)
+    if (wallet_change_password(wallet, "haha", "1234", "14725836") == -1)
     {
         printf("\n change password      error");
     }
-    wallet_change_password(wallet,"haha","12345678","14725836");
+    wallet_change_password(wallet, "haha", "12345678", "14725836");
 
     printf("\nHello, World! Verif yesult %d\n", verify);
     return 0;
