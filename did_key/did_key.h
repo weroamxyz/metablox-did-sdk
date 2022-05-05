362ca1cc3672a0025644c6b19ef618f4513cd743
VillageSecretary2020 <93245498+VillageSecretary2020@users.noreply.github.com>
2022-05-05 18:04:23 +0800

    Add files via upload

--- did_key/did_key.h
+++ did_key/did_key.h
@@ -2,14 +2,14 @@
 #define __DID_KEY_H__ 
 
 #include "unistd.h"
-#include "did_conf.h"
-#include "key_generator.h"
+#include "conf/did_conf.h"
+#include "did_key/key_generator.h"
 
 typedef struct did_key_tag {
     unsigned char  id[MAX_DID_DOC_ELEMENT_ID_LEN];
     unsigned char  type[MAX_TYPE_LEN];
     unsigned char  controller[MAX_DID_STR_LEN];
-    char  publicKeyBase58[MAX_KEY_PUBKEY_BASE58_LEN];
+    unsigned char  publicKeyHex[MAX_KEY_PUBKEY_BASE58_LEN];
     unsigned char  relationship[MAX_RELATIONSHIP_LEN];
 } did_key_t;
 
