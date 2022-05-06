c92abd8d43c76f56d24440410ef90995cd9da7d3
VillageSecretary2020 <93245498+VillageSecretary2020@users.noreply.github.com>
2022-05-06 16:16:51 +0800

    Add files via upload

--- did/did.c
+++ did/did.c
@@ -243,27 +243,16 @@ int did_sign(did_handle handle, const char *msg, size_t msg_len, char *out, size
     return key_sign(&context->key_pair, context->algo, msg, msg_len, out, out_len);
 }
 
-int did_verify(did_key_t *did_key, const char *msg, size_t msg_len, const char *sign, size_t sign_len)
+int did_verify(did_key_t *did_key, const char *msg, size_t msg_len, char *sign, size_t sign_len)
 {
     if (strcmp(did_key->type, "EcdsaSecp256k1VerificationKey2019") == 0)
     {
         key_pair_t key_pair;
         key_pair.pubkey_len = 64;
         size_t pubkey_len = 64;
-        int i = 0;
-        for (i = 0; i < strlen(did_key->publicKeyHex) / 2; i++)
-        {
-            char temp[2] = {0};
-            memcpy(temp, did_key->publicKeyHex + 2 * i, 2);
+        memcpy(&key_pair, did_key->publicKeyHex, 42);
 
-            char tmp = 0;
-            tmp = HexCharToBinChar(temp[0]);
-            tmp <<= 4;
-            tmp |= HexCharToBinChar(temp[1]);
-            memcpy(&key_pair.pubkey[i], &tmp, 1);
-        }
-        pubkey_len = i;
-        key_pair.pubkey_len = (unsigned short)pubkey_len;
+        key_pair.pubkey_len = 42;
 
         return key_verify(&key_pair, "secp256k1", msg, msg_len, sign, sign_len);
     }
@@ -290,7 +279,7 @@ did_meta_t *did_to_did_meta(did_handle handle)
         strcpy(meta->did_keys->id, "keys-1");
         strcpy(meta->did_keys->controller, context->did);
 
-        unsigned char pubKeyHex[129] = {0};
+        unsigned char pubKeyHex[45] = {0};
         did_export_pubkey(handle, pubKeyHex);
         strcpy(meta->did_keys->publicKeyHex, pubKeyHex);
 
@@ -319,67 +308,6 @@ void did_meta_destroy(did_meta_t *meta)
 
     free(meta);
 }
-/*int did_export_prikey(did_handle did,char * prikey)
-{
-    if(did==NULL)
-    {
-        return -1;
-    }
-    int len =did_serialize(did,NULL,0);
-    len=(len+15)/16*16;
-    if (len <= 0) {
-        return -1;
-    }
-    len = (len + 15) / 16  * 16;
-    char* buffer = (char*)malloc(len);
-    memset(buffer, 0, len);
-    did_serialize(did, buffer, len);
-
-    cJSON* cjson_read=NULL;
-    cJSON* cjson_key_pair=NULL;
-    cJSON* cjson_key_pair_priv=NULL;
-
-    cjson_read = cJSON_Parse(buffer);
-    if(cjson_read==NULL)
-    {
-        free(buffer);
-        return -1;
-    }
-    cjson_key_pair=cJSON_GetObjectItem(cjson_read,"key_pair");
-    if(cjson_key_pair==NULL)
-    {
-        free(buffer);
-        cJSON_Delete(cjson_read);
-        return -1;
-    }
-    cjson_key_pair_priv=cJSON_GetObjectItem(cjson_key_pair,"priv");
-    if(cjson_key_pair_priv==NULL)
-    {
-        free(buffer);
-        cJSON_Delete(cjson_read);
-        return -1;
-    }
-
-    memcpy(prikey,cjson_key_pair_priv->valuestring,strlen(cjson_key_pair_priv->valuestring));
-    free(buffer);
-    cJSON_Delete(cjson_read);
-
-    return 0;
-}
-
-// int did_export_prikey1(did_handle did, priv_key_memo_t* priv_key)
-// {
-//     if(did == NULL)
-//     {
-//         return -1;
-//     }
-//     did_context_t* context = (did_context_t*)did;
-//     strcpy(priv_key->algo, context->algo);
-//    priv_key->priv_key_len = context->key_pair.priv_len;
-//     memcpy(priv_key->priv_key, context->key_pair.priv, priv_key->priv_key_len);
-//     return 0;
-// }
-*/
 
 int did_export_prikey(did_handle did, char *out)
 {
@@ -411,7 +339,7 @@ int did_export_prikey(did_handle did, char *out)
     return 0;
 }
 
-int did_export_pubkey(did_handle did, char *out)
+int did_export_pubkey(did_handle did, char *eth_address)
 {
     if (did == NULL)
     {
@@ -419,20 +347,28 @@ int did_export_pubkey(did_handle did, char *out)
     }
 
     did_context_t *context = (did_context_t *)did;
-    // printf("\n context->key_pair.pubkey_len:%d",context->key_pair.pubkey_len);
-    char pHex[129] = {0};
 
+    char result[32] = {0};
+    SHA3_CTX sha3_ctx;
+    keccak_init(&sha3_ctx);
+    keccak_update(&sha3_ctx, context->key_pair.pubkey, context->key_pair.pubkey_len);
+    keccak_final(&sha3_ctx, result);
+
+    char pHex[64] = {0};
     int i = 0;
-    for (i = 0; i < context->key_pair.pubkey_len; i++)
+    for (i = 0; i < 32; i++)
     {
-        char strTemp[9] = {0};
-        int j = sprintf(strTemp, "%02x", context->key_pair.pubkey[i]);
+        char strTemp[3] = {0};
+        int j = sprintf(strTemp, "%02x", (unsigned char)result[i]);
         strcat(pHex, strTemp);
-        // printf("\n strTemp:%s",strTemp);
     }
-    // printf("\n pHex_len:%d",strlen(pHex));
 
-    memcpy(out, pHex, strlen(pHex));
+    char ret[42];
+    ret[0] = '0';
+    ret[1] = 'x';
+    memcpy(ret + 2, pHex + 24, 40);
+    memcpy(eth_address, ret, 42);
+
     return 0;
 }
 
--- did/did.h
+++ did/did.h
@@ -2,8 +2,9 @@
 #define __DID_H__
 
 #include "unistd.h"
-#include "did_key.h"
-#include "key_generator.h"
+#include "did_key/did_key.h"
+#include "did_key/key_generator.h"
+#include "keccak256/keccak256.h"
 
 typedef struct did_service_tag {
     char   id[MAX_ID_LEN];
@@ -35,13 +36,13 @@ void       did_destroy(did_handle handle);
 int        did_serialize(did_handle handle, char* buffer, size_t buff_len);
 did_handle did_deserialize(const char* buffer);
 int        did_sign(did_handle handle, const char* msg, size_t msg_len, char *out, size_t out_len);
-int        did_verify(did_key_t* did_key, const char* msg, size_t msg_len, const char* sign, size_t sign_len);
+int        did_verify(did_key_t* did_key, const char* msg, size_t msg_len, char* sign, size_t sign_len);
 
 did_meta_t*  did_to_did_meta(did_handle handle);
 void         did_meta_destroy(did_meta_t* meta);  
 
 //2022.4.16     meiqiu
-int         did_export_prikey(did_handle handle, char *out);
+int         did_export_prikey(did_handle handle,char *out);
 did_handle  did_import_privkey(const char *priv_key);
 char        HexCharToBinChar(char c);
 
@@ -49,7 +50,7 @@ char        HexCharToBinChar(char c);
 
 //2022.5.2
 char* getDIDstring(did_handle* did_handl);
-int did_export_pubkey(did_handle did, char *out);
+int did_export_pubkey(did_handle did, char *eth_address);
 
 
 #ifdef __cplusplus
