debe7cfaa58544536b2d8bdd1ea756be7ba966e0
VillageSecretary2020 <93245498+VillageSecretary2020@users.noreply.github.com>
2022-05-26 02:26:52 -0700

    Add files via upload

--- did_demo/did_demo/main.c
+++ did_demo/did_demo/main.c
@@ -9,6 +9,7 @@
 #include "did_wallet/did_wallet.h"
 #include "did/did.h"
 #include "string.h"
+#include "models/models.h"
 
 int main(int argc, const char *argv[])
 {
@@ -29,39 +30,78 @@ int main(int argc, const char *argv[])
     did_serialize(did2, buffer2, buff_len);
 
     char sig[64] = {0};
-    int did_sign_result = did_sign(did, "Hello, World!", strlen("Hello, World!"), sig, 64);
+    int did_sign_res = did_sign(did, "Hello, World!", strlen("Hello, World!"), sig, 64);
+    // printf("\n\n\tsig:%s,          did_sign_res:%d",sig,did_sign_res);
 
     did_meta_t *did2_meta = did_to_did_meta(did2);
 
     int verify = did_verify(did2_meta->did_keys, "Hello, World!", strlen("Hello, World!"), sig, 64);
+    // printf("\n this verify result:%d",verify);
 
-    // 2022.4.25
-    // test namelist
-    did_handle did5 = did_create("secp256k1", NULL);
-    wallet_store_did(wallet, did5, "lufi", "468217586");
+    // 2022.5.2
+    VCProof *vcProof = new_vc_proof("EcdsaSecp256k1Signature2019",
+                                    "2022-03-31T12:53:19-07:00",
+                                    "did:metablox:7rb6LjVKYSEf4LLRqbMQGgdeE8MYXkfS7dhjvJzUckEX#verification",
+                                    "Authentication");
+    // "eyJhbGciOiJFUzI1NiJ9..SnGaW3ya8MM-DXbRSFXWHM_R7Vg_3u_u1OxEfxvwXzQWNRmmC5noWvleSEM3iQdofm7towbpJ6nABQs9e1-OvA"
 
-    wallet_did_nl namelist;
-    wallet_get_namelist(wallet, &namelist);
+    char *context[2] = {"https://ns.did.ai/suites/secp256k1-2019/v1/",
+                        "https://www.w3.org/2018/credentials/v1"};
 
-    int i = 0;
-    for (i = 0; i < namelist.count; i++)
-    {
-        printf("\n namelist :%s", namelist.names[i]);
-    }
-    did_wallet_free_namelist(&namelist);
+    char *type[2] = {"PermanentResidentCard",
+                     "VerifiableCredential"};
 
-    // test impotr export private key
-    char prikey[128] = {0};
-    did_export_prikey(did, prikey);
-    printf("\n prikey:%s", prikey);
+    char *subject[6] = {"did:metablox:7rb6LjVKYSEf4LLRqbMQGgdeE8MYXkfS7dhjvJzUckEX",
+                        "John",
+                        "Jacobs",
+                        "Male",
+                        "Canada",
+                        "2022-03-22"};
 
-    did_handle did6 = did_import_privkey(prikey);
-    char buffer6[2048] = {0};
-    did_serialize(did6, buffer6, buff_len);
-    printf("\n did:%p\n did buffer:%s", did, buffer1);
-    printf("\n compare");
-    printf("\n did6:%p\n did6 buffer:%s", did6, buffer6);
+    vc_handle vc1 = new_vc(context, 2,
+                           "http://metablox.com/credentials/1",
+                           type, 2,
+                           "PermanentResidentCard",
+                           "did:metablox:sampleIssuer",
+                           "2022-03-31T12:53:19-07:00",
+                           "2032-03-31T12:53:19-07:00",
 
-    printf("\nHello, World! Verif yesult %d\n", verify);
+                           "Government of Example Permanent Resident Card", // "Government of Example Permanent Resident Card",
+                           subject, 6,
+                           *vcProof, 0);
+
+    char out[1024]={0};     
+    // did_sign(did,)  
+    vc_to_json(vc1,out);
+    // printf("\n -----------vc to json:%s", out);
+    verify=verify_vc(vc1,did);
+    vc_handle vc2= json_to_vc(out);
+
+    //*******vp
+    VPProof *vpProof = new_vp_proof("EcdsaSecp256k1Signature2019",
+                                    "2022-03-31T12:53:19-07:00",
+                                    "did:metablox:HFXPiudexfvsJBqABNmBp785YwaKGjo95kmDpBxhMMYo#verification",
+                                    "Authentication","sampleNonce");
+
+    vc_handle vc_vec[1] = {vc1};
+
+    char *context1[2] = {"https://www.w3.org/2018/credentials/v1", "https://ns.did.ai/suites/secp256k1-2019/v1/"};
+    char *type1[1] = {"VerifiablePresentation"};
+    vp_handle vp = new_vp(context1, 2,
+                          type1, 1, vc_vec, 1,
+                          "did:metablox:HFXPiudexfvsJBqABNmBp785YwaKGjo95kmDpBxhMMYo",
+                          vpProof);
+
+    // verify=verifyVP(vp,did);
+    char out2[1024]={0};
+    // convert_vp_toBytes(vp,out2);
+    printf("\n out2:%s", out2);
+    printf("\n -----------verifyVP:%d", verify);
+
+    vc_destroy(vc1);
+    vp_destroy(vp);
+
+    printf("\nHello, World! Veri result %d\n", verify);
     return 0;
 }
+
