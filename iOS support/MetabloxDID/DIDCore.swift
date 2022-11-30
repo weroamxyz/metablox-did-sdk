276ec376ab6b7b090449b6a09c0d516675636154
Tyler <tyler@Tylers-MBP.hitronhub.home>
2022-11-30 13:02:58 -0800

    verify QOS module

--- iOS support/MetabloxDID/DIDCore.swift
+++ iOS support/MetabloxDID/DIDCore.swift
@@ -402,7 +402,7 @@ public class DIDCore {
     // generate QOS (Quality Of Service)
     public func generateQOSAndSign(nonce: String, bandwidth: String, rssi: String, packetLose: String, latency: String, jws: String) -> QOSCoreModel? {
         guard let didPtr = self.loadedDIDPtr else { return nil}
-        let qos = QOSCoreModel(bandwidth: nonce, rssi: bandwidth, packetLose: rssi, latency: packetLose, nonce: latency, jws: jws)
+        let qos = QOSCoreModel(bandwidth: bandwidth, rssi: rssi, packetLose: packetLose, latency: latency, nonce: nonce, jws: jws)
         guard let qos_c = qos.toCStruct() else { return nil }
         
         qos_signature(qos_c, didPtr, &qos_c.pointee.JWSSignature.0)
